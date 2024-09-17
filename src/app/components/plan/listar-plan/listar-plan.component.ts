468ec49ccf79789857d61411a505aabdd990c642
Steven Espejo <55455142+C0C045@users.noreply.github.com>
2024-09-17 09:42:14 -0500

    Revert "Revert "feat: estilos y nuevas funcionalidades""

--- src/app/components/plan/listar-plan/listar-plan.component.ts
+++ src/app/components/plan/listar-plan/listar-plan.component.ts
@@ -37,6 +37,8 @@ export interface Plan {
   styleUrls: ['./listar-plan.component.scss']
 })
 export class ListarPlanComponent implements OnInit {
+  CODIGO_ESTADO_PR_SP!: string;
+
   displayedColumns!: string[];
   dataSource!: MatTableDataSource<any>;
   uid!: number; // id del objeto
@@ -72,7 +74,8 @@ export class ListarPlanComponent implements OnInit {
     this.banderaPlanesAccionFuncionamiento = false;
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.CODIGO_ESTADO_PR_SP = await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP');
     this.planesMostrar = [];
     if (this.banderaPlanesAccionFuncionamiento === true) {
       if (this.filtroPlan === true) {
@@ -302,6 +305,7 @@ export class ListarPlanComponent implements OnInit {
   }
 
   editar(fila: any): void {
+    this.mostrarMensajeCarga();
     this.uid = fila._id;
     this.request.get(environment.PLANES_CRUD, `plan/` + this.uid).subscribe((data: any) => {
       if (data) {
@@ -327,9 +331,9 @@ export class ListarPlanComponent implements OnInit {
   async inactivar(fila: any) {
     this.uid = fila._id;
     if (fila.activo == 'Activo') {
-      if (fila.tipo_plan_id != await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP')) {
+      if (fila.tipo_plan_id != this.CODIGO_ESTADO_PR_SP) {
         this.deleteData();
-      } else if (fila.tipo_plan_id == await this.codigosService.getId('PLANES_CRUD', 'tipo-plan', 'PR_SP')) {
+      } else if (fila.tipo_plan_id == this.CODIGO_ESTADO_PR_SP) {
         let res = {
           activo: false,
         }
